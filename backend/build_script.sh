be092b9f9b6588dd20a80a52f7d4e3fbf1503107
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2024-09-30 12:33:02 +0300

    modified:   build_script.sh

--- backend/build_script.sh
+++ backend/build_script.sh
@@ -1,3 +1,3 @@
 python manage.py migrate
 
-cd backend; echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_NAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
+# cd backend; echo "from django.contrib.auth.models import User; User.objects.create_superuser('${DJANGO_SUPERUSER_NAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
