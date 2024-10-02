42a994dcbdd26748e42e72fa25c01de308fa643d
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2024-10-02 12:07:06 +0300

    modified frontend

--- frontend/src/components/Axios.jsx
+++ frontend/src/components/Axios.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 
-const baseURL = 'http://127.0.0.1:8000/'
+// const baseURL = 'http://127.0.0.1:8000/'
+
+const isDevelopment = import.meta.env.MODE === 'development'
+const baseURL2 = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_PROD
+
 const AxiosInstance = axios.create({
-    baseURL: baseURL,
+    baseURL: baseURL2,
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
